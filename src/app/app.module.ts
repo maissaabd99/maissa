d41ad04a8d34134afb411c566ba7461acd6a0e19
Maissa <maissa.abdelwahed99@gmail.com>
2019-10-16 11:36:04 +0100

    TP3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { ProfilComponent } from './profil/profil.component';
+import {FormsModule} from'@angular/forms';
+import { TableauComponent } from './tableau/tableau.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfilComponent,
+    TableauComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
